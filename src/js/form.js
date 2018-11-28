002cf107d236322c08438cfbebda7f1cd14a320c
murilobds <murilo.santos@bsource.com.br>
2018-11-28 15:24:11 -0200

    criação do form js. e form.html foi comentado

---
+++ src/js/form.js
@@ -0,0 +1,39 @@
+<script src="https://unpkg.com/vue@2.4.2"></script>
+<script src="https://unpkg.com/vee-validate@2.0.0-rc.8"></script>
+<script type="text/javascript">
+    Vue.use(VeeValidate);
+    
+    const dictionary={
+        pt: {
+            custom: {
+                nome: {
+                    required: 'Por favor, insira seu nome'
+                },
+                email: {
+                    required: 'Por favor, insira seu e-mail',
+                    email: 'O e-mail deve ser válido'
+                },
+                mensagem: {
+                    required: 'Por favor, insira sua mensagem'
+                }
+            }
+        }
+    };
+
+    VeeValidate.Validator.updateDictionary(dictionary);
+    VeeValidate.Validator.setLocale('pt');
+
+
+    new Vue({
+        el: '#form',
+        delimiters: ['${', '}'],
+        methods: {
+            validateBeforeSubmit: function () {
+                this.$validator.validateAll();
+                if (!this.errors.any()) {
+                  this.$refs.contato.submit();
+                }
+            }
+        }
+    });
+</script>
